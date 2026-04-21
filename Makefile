5e05bc99ba0932c86344d7024296696219b82f6a
gg <gg@MacBook-Air-gg.local>
2026-04-21 13:37:25 +0200

    final working version

--- Makefile
+++ Makefile
@@ -6,22 +6,18 @@ CFLAGS      = -Wall -Wextra -Werror
 RM          = rm -f
 AR          = ar rcs
 
-# Исходники ТОЛЬКО от твоего ft_printf
 SRCS        = ft_printf.c ft_printf_utils.c
 OBJS        = $(SRCS:.c=.o)
 HEADER      = ft_printf.h
 
-# Настройки для libft
 LIBFT_DIR   = ./libft
 LIBFT       = $(LIBFT_DIR)/libft.a
 
 all: $(NAME)
 
-# Правило для компиляции самой libft (вызывает make в папке libft)
 $(LIBFT):
 	make -C $(LIBFT_DIR)
 
-# Главное правило сборки: зависит от готового libft.a и твоих .o файлов
 $(NAME): $(LIBFT) $(OBJS)
 	cp $(LIBFT) $(NAME)
 	$(AR) $(NAME) $(OBJS)
@@ -29,7 +25,6 @@ $(NAME): $(LIBFT) $(OBJS)
 %.o: %.c $(HEADER)
 	$(CC) $(CFLAGS) -c $< -o $@
 
-# Очистка должна очищать и объекты ft_printf, и объекты внутри libft
 clean:
 	$(RM) $(OBJS)
 	make clean -C $(LIBFT_DIR)
--- ft_printf.c
+++ ft_printf.c
@@ -1,47 +1,70 @@
 #include "ft_printf.h"
-
 #include "stdio.h"
 
-static int print_conversion(char c, va_list *args)
+static int	ft_putptr_hex(unsigned long n)
 {
-	int cnt;
+	int		cnt;
+	char	*base;
 
 	cnt = 0;
-	if(c == 'c')
+	base = "0123456789abcdef";
+	if (n >= 16)
+		cnt += ft_putptr_hex(n / 16);
+	cnt += ft_putchar(base[n % 16]);
+	return (cnt);
+}
+
+static int	ft_putptr(int *ptr)
+{
+	int	cnt;
+
+	cnt = 0;
+	if (ptr == NULL)
+		return (write(1, "0x0", 3)); // write(1, "(nil)", 5) for Linux
+	cnt += write(1, "0x", 2);
+	cnt += ft_putptr_hex((unsigned long)ptr);
+	return (cnt);
+}
+static int	print_conversion(char c, va_list *args)
+{
+	int	cnt;
+
+	cnt = 0;
+	if (c == 'c')
 		cnt += ft_putchar((char)va_arg(*args, int));
-	else if(c == 's')
+	else if (c == 's')
 		cnt += ft_putstr(va_arg(*args, char *));
-	else if(c == 'p')
+	else if (c == 'p')
 		cnt += ft_putptr(va_arg(*args, int *));
-	else if(c == 'd')
+	else if (c == 'd')
 		cnt += ft_putnbr(va_arg(*args, int));
-	else if(c == 'i')
+	else if (c == 'i')
 		cnt += ft_putnbr(va_arg(*args, int));
-	else if(c == 'u')
+	else if (c == 'u')
 		cnt += ft_putnbr_unsigned(va_arg(*args, unsigned));
-	else if(c == 'x' || c == 'X')
+	else if (c == 'x' || c == 'X')
 		cnt += ft_putnbr_hex(va_arg(*args, unsigned), c);
-	else if(c == '%')
+	else if (c == '%')
 		cnt += write(1, "%", 1);
 	return (cnt);
-} 
+}
 
-static int parse_text(const char *format, va_list *args)
+static int	parse_text(const char *format, va_list *args)
 {
-	int i;
-	int cnt;
+	int	i;
+	int	cnt;
 
 	i = 0;
 	cnt = 0;
-	while(format[i] != '\0')
+	while (format[i] != '\0')
 	{
-		if(format[i] == '%')
+		if (format[i] == '%')
 		{
 			i++;
 			cnt += print_conversion(format[i], args);
 		}
 		else
-			cnt += write(1,format + i, 1);
+			cnt += write(1, format + i, 1);
 		i++;
 	}
 	return (cnt);
@@ -49,14 +72,12 @@ static int parse_text(const char *format, va_list *args)
 
 int	ft_printf(const char *format, ...)
 {
-	int output_count;
-	va_list args;
+	int		output_count;
+	va_list	args;
 
-	output_count = 0;	
+	output_count = 0;
 	va_start(args, format);
-
 	output_count += parse_text(format, &args);
-
 	va_end(args);
 	return (output_count);
 }
